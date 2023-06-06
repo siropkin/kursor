1c72efd2457e9e15def27aacbfd9b3d9969ea4aa
GitHub Action <action@github.com>
2023-06-06 13:15:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/siropkin/kursor/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/siropkin/kursor/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.siropkin.kursor.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
