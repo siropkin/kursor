8a549e2d77777448c48cfe9ef02bed14721ef19d
Ivan Seredkin <ivan.seredkin@gmail.com>
2023-06-12 20:22:49 +0300

    Show language code near cursor (big change)

--- src/main/kotlin/com/github/siropkin/kursor/Kursor.kt
+++ src/main/kotlin/com/github/siropkin/kursor/Kursor.kt
@@ -1,48 +1,66 @@
 package com.github.siropkin.kursor
 
-import com.intellij.openapi.editor.CaretVisualAttributes
-import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.*
+import com.intellij.openapi.editor.event.CaretEvent
+import com.intellij.openapi.editor.event.CaretListener
 import java.awt.Color
+import java.awt.Graphics
+import java.awt.Point
+import java.awt.event.ComponentEvent
+import java.awt.event.ComponentListener
 import java.awt.im.InputContext
+import javax.swing.JComponent
 
-class Kursor {
-    private var defaultCaretVisualAttributes: CaretVisualAttributes? = null
-    private var customCaretVisualAttributes: CaretVisualAttributes = CaretVisualAttributes(Color.RED, CaretVisualAttributes.Weight.HEAVY, CaretVisualAttributes.Shape.BLOCK, 1.0f)
+class Kursor(private var editor: Editor): JComponent(), ComponentListener, CaretListener {
+    init {
+        val caretModel: CaretModel = editor.caretModel
+        caretModel.addCaretListener(this)
 
-    // get current language
+        editor.contentComponent.add(this)
+        editor.contentComponent.repaint()
+
+        this.isVisible = true
+        this.bounds = editor.contentComponent.bounds
+    }
+
     private fun getLanguage(): String {
         val context: InputContext = InputContext.getInstance()
         return context.locale.toString().substring(0, 2)
     }
 
-    // get caret visual attributes
-    private fun getCaretVisualAttributes(): CaretVisualAttributes {
-        val caret = EditorFactory.getInstance().allEditors.first().caretModel.primaryCaret
-        return caret.visualAttributes
+    private fun getCaretPosition(caret: Caret): Point {
+        return caret.editor.visualPositionToXY(caret.visualPosition)
     }
 
-    // set caret visual attributes
-    private fun setCaretVisualAttributes(caretVisualAttributes: CaretVisualAttributes) {
-        EditorFactory.getInstance().allEditors.forEach {
-            val caret = it.caretModel.primaryCaret
-            caret.visualAttributes = caretVisualAttributes
-        }
-    }
-
-    // save default caret visual attributes
-    fun saveDefaultCaretVisualAttributes() {
-        if (defaultCaretVisualAttributes == null) {
-            defaultCaretVisualAttributes = getCaretVisualAttributes()
-        }
-    }
-
-    // update caret visual attributes based on current language
-    fun updateCaretVisualAttributes() {
+    override fun paint(g: Graphics) {
+        super.paint(g)
         val language = getLanguage()
-        if (language == "en") {
-            setCaretVisualAttributes(defaultCaretVisualAttributes!!)
-        } else {
-            setCaretVisualAttributes(customCaretVisualAttributes)
+        val fontSize = editor.colorsScheme.editorFontSize * 1.2f
+        val defaultFontColor = editor.colorsScheme.defaultForeground
+        val fontColor = Color(defaultFontColor.red, defaultFontColor.green, defaultFontColor.blue, 80)
+        editor.caretModel.allCarets.forEach { caret ->
+            g.font = g.font.deriveFont(fontSize)
+            val caretPosition = getCaretPosition(caret)
+            g.color = fontColor
+            g.drawString(language, caretPosition.x + 4, caretPosition.y + 4)
         }
+    }
+
+    override fun componentResized(e: ComponentEvent?) {
+        this.bounds = editor.contentComponent.bounds
+    }
+
+    override fun componentMoved(e: ComponentEvent?) {
+        this.bounds = editor.contentComponent.bounds
+    }
+
+    override fun componentShown(e: ComponentEvent?) {
+    }
+
+    override fun componentHidden(e: ComponentEvent?) {
+    }
+
+    override fun caretPositionChanged(e: CaretEvent) {
+        repaint()
     }
 }
--- src/main/kotlin/com/github/siropkin/kursor/KursorStartupActivity.kt
+++ src/main/kotlin/com/github/siropkin/kursor/KursorStartupActivity.kt
@@ -3,32 +3,59 @@ package com.github.siropkin.kursor
 import com.intellij.ide.FrameStateListener
 import com.intellij.ide.IdeEventQueue
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.event.EditorFactoryEvent
+import com.intellij.openapi.editor.event.EditorFactoryListener
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
 import java.awt.event.KeyEvent
 
 class KursorStartupActivity: StartupActivity {
-    private var kursor: Kursor = Kursor()
+    private val kursors = mutableMapOf<Editor, Kursor>()
 
     override fun runActivity(project: Project) {
-        kursor.saveDefaultCaretVisualAttributes()
-        kursor.updateCaretVisualAttributes()
+        // add kursor to all existing editors
+        val editors: Array<Editor> = EditorFactory.getInstance().allEditors
+        for (editor in editors) {
+            val kursor = Kursor(editor)
+            kursors[editor] = kursor
+        }
+
+        // add editor factory listener to track editor creation and deletion
+        EditorFactory.getInstance().addEditorFactoryListener(object : EditorFactoryListener {
+            override fun editorCreated(event: EditorFactoryEvent) {
+                val editor: Editor? = event.editor
+                if (editor != null) {
+                    val kursor = Kursor(editor)
+                    kursors[editor] = kursor
+                }
+
+            }
+
+            override fun editorReleased(event: EditorFactoryEvent) {
+                val editor: Editor? = event.editor
+                if (editor != null) {
+                    kursors.remove(editor)
+                }
+            }
+        }, project)
 
         // TODO: Add listener for cases when user changes language using OS UI
-
         // add frame state listener
         val connection = ApplicationManager.getApplication().messageBus.connect()
         connection.subscribe(FrameStateListener.TOPIC, object : FrameStateListener {
             override fun onFrameActivated() {
-                kursor.updateCaretVisualAttributes()
+                kursors.forEach { (_, kursor) -> kursor.repaint() }
             }
         })
 
-        // add key listener
         // TODO: Fix Win + Space issue: if Win released after Space, then Space is not registered
+        // add key listener
         IdeEventQueue.getInstance().addDispatcher({ event ->
             if (event is KeyEvent) {
-                kursor.updateCaretVisualAttributes()
+                // repaint all kursors
+                kursors.forEach { (_, kursor) -> kursor.repaint() }
             }
             false
         }, project)
