bb370e58441c497e247946d817e9e83d56721eb9
Ivan Seredkin <ivan.seredkin@gmail.com>
2023-06-13 10:11:57 +0300

    Code refactoring

--- src/main/kotlin/com/github/siropkin/kursor/Kursor.kt
+++ src/main/kotlin/com/github/siropkin/kursor/Kursor.kt
@@ -1,50 +1,34 @@
 package com.github.siropkin.kursor
 
+import com.intellij.codeInsight.codeVision.ui.visibleAreaChanged
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.*
 import com.intellij.openapi.editor.event.CaretEvent
 import com.intellij.openapi.editor.event.CaretListener
-import java.awt.Graphics
-import java.awt.Point
+import com.intellij.openapi.editor.event.VisibleAreaEvent
+import java.awt.*
 import java.awt.event.ComponentEvent
 import java.awt.event.ComponentListener
+import java.awt.event.KeyEvent
 import java.awt.im.InputContext
+import java.util.*
 import javax.swing.JComponent
 
-// TODO: Add blinking cursor
+// TODO: show and hide g when caret is blinking
+//val blinkPeriod = editor.settings.caretBlinkPeriod
+//if (blinkPeriod > 0) { }
+
 class Kursor(private var editor: Editor): JComponent(), ComponentListener, CaretListener {
+    // save init scroll position
+    private val initLocation = editor.scrollingModel.visibleArea.location
+
     init {
+        editor.contentComponent.add(this)
+
         this.isVisible = true
         this.bounds = editor.contentComponent.bounds
 
         editor.caretModel.addCaretListener(this)
-
-        editor.contentComponent.add(this)
-        editor.contentComponent.repaint()
-    }
-
-    private fun getLanguage(): String {
-        val context: InputContext = InputContext.getInstance()
-        return context.locale.toString().substring(0, 2)
-    }
-
-    private fun getCaretPosition(caret: Caret): Point {
-        return caret.editor.visualPositionToXY(caret.visualPosition)
-    }
-
-    override fun paint(g: Graphics) {
-        super.paint(g)
-        val language = getLanguage()
-        // val fontSize = editor.colorsScheme.editorFontSize * 1.2f
-        // val defaultFontColor = editor.colorsScheme.defaultForeground
-        // val fontColor = Color(defaultFontColor.red, defaultFontColor.green, defaultFontColor.blue, 80)
-        editor.caretModel.allCarets.forEach { caret ->
-            // g.font = g.font.deriveFont(fontSize)
-            // g.color = fontColor
-            val caretPosition = getCaretPosition(caret)
-            val offsetX = 4
-            val offsetY = (editor.colorsScheme.editorFontSize2D / 2).toInt() + 1
-            g.drawString(language, caretPosition.x + offsetX, caretPosition.y + offsetY)
-        }
     }
 
     override fun componentResized(e: ComponentEvent?) {
@@ -56,16 +40,58 @@ class Kursor(private var editor: Editor): JComponent(), ComponentListener, Caret
     }
 
     override fun componentShown(e: ComponentEvent?) {
-        this.bounds = editor.contentComponent.bounds
-        this.isVisible = true
     }
 
     override fun componentHidden(e: ComponentEvent?) {
-        this.bounds = editor.contentComponent.bounds
-        this.isVisible = false
     }
 
     override fun caretPositionChanged(e: CaretEvent) {
         repaint()
+    }
+
+    private fun getLanguage(): String {
+        val context: InputContext = InputContext.getInstance()
+        return context.locale.toString().substring(0, 2)
+    }
+
+    private fun isCapsLockOn(): Boolean {
+        val toolkit = Toolkit.getDefaultToolkit()
+        return toolkit.getLockingKeyState(KeyEvent.VK_CAPS_LOCK)
+    }
+
+    private fun getPoint(position: VisualPosition, editor: Editor): Point {
+        val p: Point = editor.visualPositionToXY(position)
+        // translate to scroll position
+        val scrollingModel = editor.scrollingModel
+        p.translate(scrollingModel.horizontalScrollOffset, scrollingModel.verticalScrollOffset)
+        // translate to current scroll location
+        val location = scrollingModel.visibleArea.location
+        p.translate(-location.x, -location.y)
+        // translate to init scroll location
+        p.translate(initLocation.x, initLocation.y)
+        return p
+    }
+
+    private fun getCaretPosition(caret: Caret): Point {
+        return getPoint(caret.visualPosition, caret.editor)
+    }
+
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+        var language = getLanguage()
+        val isCapsLockOn = isCapsLockOn()
+        if (isCapsLockOn) {
+            language = language.uppercase(Locale.getDefault())
+        }
+        val offsetX = 4
+        var offsetY = (editor.colorsScheme.editorFontSize2D / 2).toInt()
+        // TODO: fix offsetY for different fonts
+        // val bounds = g.fontMetrics.getStringBounds(language, g)
+        offsetY += if (isCapsLockOn) { 5 } else { 2 }
+        editor.caretModel.allCarets.forEach { caret ->
+            val caretPosition = getCaretPosition(caret)
+            g.color = caret.visualAttributes.color
+            g.drawString(language, caretPosition.x + offsetX, caretPosition.y + offsetY)
+        }
     }
 }
--- src/main/kotlin/com/github/siropkin/kursor/KursorStartupActivity.kt
+++ src/main/kotlin/com/github/siropkin/kursor/KursorStartupActivity.kt
@@ -3,6 +3,7 @@ package com.github.siropkin.kursor
 import com.intellij.ide.FrameStateListener
 import com.intellij.ide.IdeEventQueue
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.EditorFactoryEvent
@@ -11,6 +12,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import java.awt.event.KeyEvent
 
+// TODO: Add listener for cases when user changes language using OS UI
 class KursorStartupActivity: StartupActivity {
     private val kursors = mutableMapOf<Editor, Kursor>()
 
@@ -18,27 +20,36 @@ class KursorStartupActivity: StartupActivity {
         // add kursor to all existing editors
         val editors: Array<Editor> = EditorFactory.getInstance().allEditors
         for (editor in editors) {
-            kursors[editor] = Kursor(editor)
+            if (kursors[editor] == null) {
+                thisLogger().warn("runActivity - existing: $editor.contentComponent.bounds")
+                kursors[editor] = Kursor(editor)
+            }
         }
 
         // add editor factory listener to track editor creation and deletion
         EditorFactory.getInstance().addEditorFactoryListener(object : EditorFactoryListener {
             override fun editorCreated(event: EditorFactoryEvent) {
                 val editor: Editor = event.editor
-                kursors[editor] = Kursor(editor)
+                if (kursors[editor] == null) {
+                    thisLogger().warn("runActivity - editorCreated: $editor.contentComponent.bounds")
+                    kursors[editor] = Kursor(editor)
+                }
             }
 
             override fun editorReleased(event: EditorFactoryEvent) {
                 val editor: Editor = event.editor
-                kursors.remove(editor)
+                if (kursors[editor] != null) {
+                    thisLogger().warn("runActivity - editorReleased: $editor.contentComponent.bounds")
+                    kursors.remove(editor)
+                }
             }
         }, project)
 
-        // TODO: Add listener for cases when user changes language using OS UI
         // add frame state listener
         val connection = ApplicationManager.getApplication().messageBus.connect()
         connection.subscribe(FrameStateListener.TOPIC, object : FrameStateListener {
             override fun onFrameActivated() {
+                thisLogger().warn("runActivity - onFrameActivated")
                 kursors.forEach { (_, kursor) -> kursor.repaint() }
             }
         })
@@ -47,6 +58,7 @@ class KursorStartupActivity: StartupActivity {
         // add key listener
         IdeEventQueue.getInstance().addDispatcher({ event ->
             if (event is KeyEvent) {
+                thisLogger().warn("runActivity - KeyEvent")
                 kursors.forEach { (_, kursor) -> kursor.repaint() }
             }
             false
