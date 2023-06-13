9ea020a7dfa87c57a1780cbd35f0ce8884bbf788
Ivan Seredkin <ivan.seredkin@gmail.com>
2023-06-13 15:55:56 +0300

    Add alpha and positions

--- src/main/kotlin/com/github/siropkin/kursor/Kursor.kt
+++ src/main/kotlin/com/github/siropkin/kursor/Kursor.kt
@@ -1,6 +1,7 @@
 package com.github.siropkin.kursor
 
 import com.intellij.openapi.editor.*
+import com.intellij.openapi.editor.colors.EditorColors
 import com.intellij.openapi.editor.event.CaretEvent
 import com.intellij.openapi.editor.event.CaretListener
 import java.awt.*
@@ -15,7 +16,18 @@ import javax.swing.JComponent
 //val blinkPeriod = editor.settings.caretBlinkPeriod
 //if (blinkPeriod > 0) { }
 
+object Position {
+    const val TOP = "top"
+    const val BOTTOM = "bottom"
+    const val MIDDLE = "middle"
+}
+
 class Kursor(private var editor: Editor): JComponent(), ComponentListener, CaretListener {
+    private val defaultLanguage = "en"
+    private val defaultAlpha = 180
+    private val defaultPosition = Position.TOP
+    // TODO: Add option to change color
+
     init {
         editor.contentComponent.add(this)
         isVisible = true
@@ -61,6 +73,14 @@ class Kursor(private var editor: Editor): JComponent(), ComponentListener, Caret
         return p
     }
 
+    // get caret height
+    private fun getCaretHeight(): Int {
+        val caret = editor.caretModel.primaryCaret
+        val p1 = editor.visualPositionToXY(caret.visualPosition)
+        val p2 = editor.visualPositionToXY(VisualPosition(caret.visualPosition.line + 1, caret.visualPosition.column))
+        return p2.y - p1.y
+    }
+
     private fun getMyBounds(): Rectangle {
         val area = editor.scrollingModel.visibleArea
         return Rectangle(area.x, area.y, area.width, area.height)
@@ -73,16 +93,26 @@ class Kursor(private var editor: Editor): JComponent(), ComponentListener, Caret
         }
         var language = getLanguage()
         val isCapsLockOn = isCapsLockOn()
+        if (language == defaultLanguage && !isCapsLockOn) {
+            return
+        }
         if (isCapsLockOn) {
             language = language.uppercase(Locale.getDefault())
         }
         val offsetX = 4
-        var offsetY = (editor.colorsScheme.editorFontSize2D / 2).toInt()
-        // TODO: fix offsetY for different fonts
-        // val bounds = g.fontMetrics.getStringBounds(language, g)
-        offsetY += if (isCapsLockOn) { 5 } else { 2 }
+        var offsetY = 0
+        when (defaultPosition) {
+            Position.TOP -> offsetY = 0
+            // TODO: fix offsetY for different fonts
+            Position.BOTTOM -> offsetY = getCaretHeight() + if (isCapsLockOn) { 6 } else { 3 }
+            Position.MIDDLE -> offsetY = editor.colorsScheme.editorFontSize
+        }
         editor.caretModel.allCarets.forEach { caret ->
             val caretPosition = getCaretPosition(caret)
+            val color = caret.visualAttributes.color ?: editor.colorsScheme.getColor(EditorColors.CARET_COLOR)
+            if (color != null) {
+                g.color = Color(color.red, color.green, color.blue, defaultAlpha)
+            }
             g.drawString(language, caretPosition.x + offsetX, caretPosition.y + offsetY)
         }
     }
